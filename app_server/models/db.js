878fd6231cca0bbaaccc2cbc5c63ca8dbbfcb7a3
Ross Moug <ross.moug@gmail.com>
2018-04-03 19:42:24 +0100

    Creating location models and MongoDB integration

---
+++ app_server/models/db.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbUri = 'mongodb://localhost/Loc8r';
+
+if (process.env.NODE_ENV === 'production') {
+    dbUri = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbUri);
+
+var readline = require('readline');
+if (process.platform === "win32") {
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on("SIGINT", function() {
+        process.emit("SIGINT");
+    });
+}
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbUri);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback;
+    })
+};
+
+// For Nodemon restarts
+process.once('SIGUSER2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSER2');
+    });
+});
+
+// For app termination
+process.once('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+// For Heroku app termination
+process.once('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function() {
+        process.exit(0);
+    });
+});
